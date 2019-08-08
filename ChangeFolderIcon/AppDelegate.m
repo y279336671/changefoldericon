e26c78470e7e9b122593229ece69709ab803ec52
yanghe <yanghe@autohome.com.cn>
2019-08-08 13:45:27 +0800

    优化

--- ChangeFolderIcon/AppDelegate.m
+++ ChangeFolderIcon/AppDelegate.m
@@ -10,19 +10,30 @@
 #import "Tools.h"
 #import "MyWindow.h"
 #import "MainViewController.h"
-@interface AppDelegate () <NSApplicationDelegate,NSMenuDelegate>
-@property (nonatomic,strong) NSStatusItem *statusItem;
+
+@interface AppDelegate () <NSApplicationDelegate, NSMenuDelegate>
+@property(nonatomic, strong) NSStatusItem *statusItem;
+@property (nonatomic, strong)NSWindow *window;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+
+    self.window = [[MyWindow alloc] initWithContentRect:NSMakeRect(50, 100, 200, 300) styleMask:NSWindowStyleMaskBorderless | NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable backing:NSBackingStoreBuffered defer:YES];
+    self.window.contentViewController = [[MainViewController alloc] init];
+    [self.window setTitle:@"test1"];
+
     [self addStatusItem];
-    [self openApp];
+    //所有活跃应用
+//    NSArray* arrayAppList = [[NSWorkspace sharedWorkspace] runningApplications];
+
 }
 
+
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-   
+
+
 }
 
 - (void)addStatusItem {
@@ -41,48 +52,79 @@
 
     button.target = self;
 
-//    button.action = @selector(handleButtonClick:);
+    button.action = @selector(handleButtonClick:);
 
-    NSString *appName = [[Tools getAppInfo]objectForKey:@"CFBundleName"];
-    
+
+}
+
+- (void)handleButtonClick:(NSStatusBarButton *)sender {
+    NSLog(@"handleButtonClick");
+    NSString *bundleId = [[Tools getAppInfo] objectForKey:@"CFBundleIdentifier"];
+    NSArray *array = [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleId];
+    if ([array count] > 0) {
+        NSRunningApplication *runningApp = [array objectAtIndex:0];
+        if (!runningApp.isActive) {
+            [self openApp];
+
+        }
+        [self bindSubMenu];
+    }
+}
+
+- (void)bindSubMenu {
+    NSLog(@"bindSubMenu");
+    NSString *appName = [[Tools getAppInfo] objectForKey:@"CFBundleName"];
     NSMenu *subMenu = [[NSMenu alloc] initWithTitle:@""];
-    [subMenu addItemWithTitle:[NSString stringWithFormat:@"打开%@",appName] action:@selector(openApp) keyEquivalent:@""];
-    [subMenu addItemWithTitle:[NSString stringWithFormat:@"退出%@",appName] action:@selector(exitApp) keyEquivalent:@""];
-
+    [subMenu addItemWithTitle:[NSString stringWithFormat:@"打开%@", appName] action:@selector(openApp) keyEquivalent:@""];
+    [subMenu addItemWithTitle:[NSString stringWithFormat:@"退出%@", appName] action:@selector(exitApp) keyEquivalent:@""];
     self.statusItem.menu = subMenu;
     self.statusItem.menu.delegate = self;
-   
 }
 
-- (void)menuWillOpen:(NSMenu *)menu{
-     //todo  第一次点击显示程序主界面 第二次显示菜单
+- (void)unBindSubMenu {
+    NSLog(@"unBindSubMenu");
+    self.statusItem.menu.delegate = nil;
+    self.statusItem.menu = nil;
 }
 
-- (void)menu:(NSMenu *)menu willHighlightItem:(nullable NSMenuItem *)item{
-   
+- (void)menuWillOpen:(NSMenu *)menu {
+    NSLog(@"menuWillOpen");
+}
+
+- (void)menuDidClose:(NSMenu *)menu {
+    NSLog(@"menuDidClose");
+    NSString *bundleId = [[Tools getAppInfo] objectForKey:@"CFBundleIdentifier"];
+    NSArray *array = [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleId];
+    if ([array count] > 0) {
+        NSRunningApplication *runningApp = [array objectAtIndex:0];
+        if (runningApp.isActive) {
+            [self bindSubMenu];
+
+        } else{
+            [self unBindSubMenu];
+        }
+
+    }
+
 }
 
 //显示主界面
--(void)openApp{
-    NSWindow *window = [[MyWindow alloc] initWithContentRect:NSMakeRect(50, 100, 200, 300)styleMask:NSWindowStyleMaskBorderless|NSWindowStyleMaskTitled|NSWindowStyleMaskClosable|NSWindowStyleMaskMiniaturizable backing:NSBackingStoreBuffered defer:YES];
-    
-    window.contentViewController = [[MainViewController alloc]init];
-    [window setTitle:@"test1"];
-    [window makeKeyAndOrderFront:nil];
+- (void)openApp {
+
+    [self.window makeKeyAndOrderFront:nil];
     [self bringToFront];
 }
 
 //退出应用
--(void)exitApp{
+- (void)exitApp {
     [[NSApplication sharedApplication] terminate:self];
 }
 
 //应用显示在最前面
--(void)bringToFront{
-    NSString *bundleId =[[Tools getAppInfo]objectForKey:@"CFBundleIdentifier"];
+- (void)bringToFront {
+    NSString *bundleId = [[Tools getAppInfo] objectForKey:@"CFBundleIdentifier"];
     NSArray *array = [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleId];
-    if ([array count] > 0)
-    {
+    if ([array count] > 0) {
         NSRunningApplication *runningApp = [array objectAtIndex:0];
         [runningApp activateWithOptions:NSApplicationActivateIgnoringOtherApps];
     }
